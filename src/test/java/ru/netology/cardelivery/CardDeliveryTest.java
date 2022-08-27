ee39677d81995ca61aeab7f409fc25f8c1ae646c
Roman Medvedev <romankazan88@gmail.com>
2022-08-27 12:31:42 +0500

    test modification

--- src/test/java/ru/netology/cardelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/cardelivery/CardDeliveryTest.java
@@ -1,4 +1,4 @@
-package ru.netology.CardDelivery;
+package ru.netology.cardelivery;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
@@ -6,34 +6,40 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardDeliveryTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void mustSubmitTheForm() {
         Configuration.holdBrowserOpen = true;
-        Date currentDate = new Date();
-        SimpleDateFormat ft = new SimpleDateFormat ("dd-MM-yyyy");
-        Calendar c = Calendar.getInstance();
-        c.setTime(currentDate);
-        c.add(Calendar.DATE, 3);
-        String newCurrentDate = ft.format(c.getTime());
+        String planningDate = generateDate(5);
 
         open("http://localhost:9999/");
         $(By.cssSelector("[data-test-id=city] input")).setValue("Казань");
-        $(By.cssSelector("[data-test-id=date] input")).sendKeys(Keys.BACK_SPACE);
-        $("div [data-test-id='date'] input").setValue(newCurrentDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("div [data-test-id='date'] input").setValue(planningDate);
         $(By.cssSelector("[data-test-id=name] input")).setValue("Иванов Иван");
         $(By.cssSelector("[data-test-id=phone] input")).setValue("+79999998888");
         $(By.className("checkbox__box")).click();
         $(By.className("button__text")).click();
         $x("//*[contains(text(),'Успешно')]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
+
+
 }
+
+
